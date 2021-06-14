4d00b559dc7425b66db788649ccfbeee8a7278d0
JillKlatt <klatt.jill@gmail.com>
2021-06-14 18:43:45 -0400

    commit tree 1148fb1988ac2027149c95e860437b80d15c3b57

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,17 @@
 export function managePresents(state, action){
+
+    state={
+    numberOfPresents: 0
+    }
+
+    switch (action.type) {
+        case 'INCREASE':
+            return {numberOfPresents: state.numberOfPresents + 1}
+        case 'DECREASE':
+            return {numberOfPresents: state.numberOfPresents - 1}
+        default:
+            return state;
+    }
+
+
 }
